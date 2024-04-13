b5887db4df00376d1165d078aea55593e515d1a7
wadevs14161 <wadevs14161@gmail.com>
2024-04-13 22:16:11 +0800

    delete app

--- azureproject/development.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'],
-    dbname=os.environ['DBNAME']
-)
