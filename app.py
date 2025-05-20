6fee8c6491c51028f106572918e06c3888cb7ff8
LUO WEN SHIN <wadevs14161@gmail.com>
2025-05-20 17:29:18 +0100

    update port running

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ def message_text(event):
 #     return "OK"
 
 if __name__ == '__main__':
-    
-    app.run(debug=True, port=8080)
+    # Running on port 5000, let nginx (port 8080) to proxy to this port
+    app.run(debug=True, host='0.0.0.0', port=5000)
