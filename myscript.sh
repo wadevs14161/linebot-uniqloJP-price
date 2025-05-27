66c4e54cbfecdca4e28881ed3deb7e0cddc62bac
LUO WEN SHIN <wadevs14161@gmail.com>
2025-05-27 13:46:41 +0100

    remove credentials

--- myscript.sh
+++ myscript.sh
@@ -1,12 +1,12 @@
 # Run the following command to set the environment variables
 # $ source myscript.sh
-export LINE_CHANNEL_SECRET="37d24f448086640d7824cb363f5fd1c1"
-export LINE_CHANNEL_ACCESS_TOKEN="oGbZ9+xp5AK2S9A1jG7nzUpiH13skFHh1pBlJ2A0oLpu5wIQhQdMmj4tbqC/+2F/kkw5T+12bUSQONaPzcWeC3VCzPQYSpQC9SoNv2lkqGzSr1IHySlWnMogK7rBs9ClkX07RoAkvH6nZ0FbodlSLgdB04t89/1O/w1cDnyilFU="
+export LINE_CHANNEL_SECRET=""
+export LINE_CHANNEL_ACCESS_TOKEN=""
 echo $LINE_CHANNEL_SECRET
 echo $LINE_CHANNEL_ACCESS_TOKEN
 
 # Load Virtual environment
-source .venv/bin/activate
+source venv/bin/activate
 
 # Start flask app in the background using cmd "nohup python app.py &"
 nohup python app.py &
